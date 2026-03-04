8ab2dfc7051e0cd5a004aacc90f8cc8a26d72a47
Muneeb Zain <muneebzain008@gmail.com>
2026-03-04 18:25:44 +0500

    Initial Commit

---
+++ ResumeSenseAI/ResumeSenseAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeSenseAIApp.swift
+//  ResumeSenseAI
+//
+//  Created by Muneeb Zain on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ResumeSenseAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
